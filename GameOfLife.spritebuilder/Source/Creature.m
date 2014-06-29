e903a21b846cf94b3a330594842df761ad793688
Rbabu18 <rbabu@Rameshs-MacBook-Pro.local>
2014-06-29 22:05:17 +1000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Creature.m
+++ GameOfLife.spritebuilder/Source/Creature.m
@@ -7,6 +7,9 @@
 //
 
 #import "Creature.h"
+#import "CCAnimation.h"
+
+#define MAX_ANIMATION_FRAMES 5
 
 @implementation Creature
 - (instancetype)initCreature {
@@ -27,5 +30,26 @@
     
     // 'visible' is a property of any class that inherits from CCNode. CCSprite is a subclass of CCNode, and Creature is a subclass of CCSprite, so Creatures have a visible property
     self.visible = _isAlive;
+}
+
+-(void) animateCreature {
+    
+    NSMutableArray *animationFrames = [NSMutableArray array];
+    
+    for(int i = 0; i <= MAX_ANIMATION_FRAMES; ++i)
+    {
+        CCSpriteFrame *spriteFrame = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName: [NSString stringWithFormat:@"ccbResources/GameOfLifeAssets/Assets/bubble%d.png", i]];
+        [animationFrames addObject: spriteFrame];
+    }
+    
+    
+    //Create an animation from the set of frames you created earlier
+    CCAnimation *animation = [CCAnimation animationWithSpriteFrames:animationFrames delay:1.0f];
+    
+    //Create an action with the animation that can then be assigned to a sprite
+    CCActionAnimate *animationAction = [CCActionAnimate actionWithAnimation:animation];
+    
+    CCActionRepeatForever *repeatingAnimation = [CCActionRepeatForever actionWithAction:animationAction];
+    [self runAction:repeatingAnimation];
 }
 @end
